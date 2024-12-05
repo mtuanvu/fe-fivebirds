70ed34dede6f1af831126ae0d6b05fa899dab3f2
mtuanvu <143334278+mtuanvu@users.noreply.github.com>
2024-12-05 13:55:22 +0700

    a

--- src/api/userApi/UserApi.js
+++ src/api/userApi/UserApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const userApi = axios.create({
-  baseURL: "https://fivebirds-f9g8a3bzfxdfhkcc.japaneast-01.azurewebsites.net/api/v1/users/",
+  baseURL: "https://fivebirds-f9g8a3bzfxdfhkcc.japaneast-01.azurewebsites.net/api/v1/users",
   withCredentials: true,
 });
 
